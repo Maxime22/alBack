d90b9536989e37120a05f5eae17de9e1d1df7184
Maxime <sabouretmaxime@gmail.com>
2020-06-22 23:11:37 +0200

    front started

--- models/page.js
+++ models/page.js
@@ -4,7 +4,8 @@ const pageSchema = mongoose.Schema({
     mainImgUrl: { type: String, required: false },
     typeOfTemplate: { type: String, required: true },
     title: { type: String, required: true },
-    content: { type: String, required: true }
+    content: { type: String, required: true },
+    content2: { type: String, required: false }
 });
 
 module.exports = mongoose.model('Page', pageSchema);
