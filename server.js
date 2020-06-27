925da1646068f6c8090121c8623817f03e49b00a
Maxime <sabouretmaxime@gmail.com>
2020-06-27 16:09:17 +0200

    test https one more time but with http

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const http = require('http');
 const app = require('./app');
 
-// const fs = require('fs');
-// const https = require('https');
-
 // test HTTPS Certificate
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
-// const credentials = {
-// 	key: privateKey,
-// 	cert: certificate,
-// 	ca: ca
-// };
+const fs = require('fs');
+const https = require('https');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -49,10 +48,10 @@ const errorHandler = error => {
     }
 };
 
-// DEV
+// normal http
 const server = http.createServer(app);
 // test HTTPS
-// const httpserver = https.createServer(credentials, app);
+const httpserver = https.createServer(credentials, app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
@@ -62,3 +61,4 @@ server.on('listening', () => {
 });
 
 server.listen(port);
+httpserver.listen(443);
