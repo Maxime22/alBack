713f65b1884e8c228076bcbed9b3283ed7f798bf
Maxime <sabouretmaxime@gmail.com>
2020-06-27 15:52:02 +0200

    https deleted from api...

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-// const http = require('http');
+const http = require('http');
 const app = require('./app');
 
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 
 // test HTTPS Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: ca
+// };
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -29,36 +29,36 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-// const errorHandler = error => {
-//     if (error.syscall !== 'listen') {
-//         throw error;
-//     }
-//     const address = server.address();
-//     const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-//     switch (error.code) {
-//         case 'EACCES':
-//             console.error(bind + ' requires elevated privileges.');
-//             process.exit(1);
-//             break;
-//         case 'EADDRINUSE':
-//             console.error(bind + ' is already in use.');
-//             process.exit(1);
-//             break;
-//         default:
-//             throw error;
-//     }
-// };
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
 
-// DEV ?
-// const server = http.createServer(app);
+// DEV
+const server = http.createServer(app);
 // test HTTPS
-const server = https.createServer(credentials, app);
+// const httpserver = https.createServer(credentials, app);
 
-// server.on('error', errorHandler);
-// server.on('listening', () => {
-//     const address = server.address();
-//     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-//     console.log('Listening on ' + bind);
-// });
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
 
 server.listen(port);
