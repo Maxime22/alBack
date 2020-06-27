f75da67ffa055d16c9e4eac2538b78479a7706b3
Maxime <sabouretmaxime@gmail.com>
2020-06-27 16:20:26 +0200

    https...

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = require('./app');
 // test HTTPS Certificate
 const fs = require('fs');
 const https = require('https');
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/annelods.fr/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/annelods.fr/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/annelods.fr/chain.pem', 'utf8');
 const credentials = {
 	key: privateKey,
 	cert: certificate,
